23afead525f1cbbc47fedd1e8f5a33bc22a4701a
Fiona <fiona.nugent7@gmail.com>
2023-02-22 22:45:58 +0000

    implement convert command, separated out args and commands from main

--- linktune/cli/main.py
+++ linktune/cli/main.py
@@ -1,10 +1,24 @@
 from arguments import create_parser
 from linktune.api.convert import Convert
 
-# create command-line argument parser
-parser = create_parser()
-args = parser.parse_args()
+# # create command-line argument parser
+# parser = create_parser()
+# args = parser.parse_args()
 
-converter = Convert()
-track = converter.convert_link(args.link, args.target)
-print(track['info'], track['url'])
+# converter = Convert()
+# track = converter.convert_link(args.link, args.target)
+# print(track['info'], track['url'])
+
+def main():
+    parser = create_parser()
+
+    args = parser.parse_args()
+
+    if not args.command:
+        parser.print_help()
+        return
+
+    args.func(args)
+
+if __name__ == '__main__':
+    main()
