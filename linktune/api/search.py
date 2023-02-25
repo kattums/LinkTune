3b71741d368243ccb3608800ec06aeb32bba16b4
Fiona <fiona.nugent7@gmail.com>
2023-02-25 11:38:43 +0000

    add requirements.txt

--- linktune/api/search.py
+++ linktune/api/search.py
@@ -2,10 +2,7 @@ from linktune.api.tidal import Tidal
 from linktune.api.spotify import Spotify
 from linktune.config.config import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET
 
-# given an artist name, track title, and target service, search should locate track and return links. default value for target is all.
-# if 'all', then return links for all services
-
-# TODO: implement map of services for quicker look up; add "all" option which is the default; remove api keys from code
+# TODO: implement map of services for quicker look up; add "all" option which is the default
 def search_track(artist, title, service='all'):
     info = {'title': title, 'artist': artist}
     if service == 'spotify':
