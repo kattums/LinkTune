f343fcd38391bb162e919d611538b5a80e91b0d3
Fiona <fiona.nugent7@gmail.com>
2023-02-25 23:03:27 +0000

    refactor search to use api map

--- linktune/api/search.py
+++ linktune/api/search.py
@@ -7,17 +7,33 @@ from linktune.config.config import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET
 # TODO: implement map of services for quicker look up; add "all" option which is the default
 def search_track(artist, title, service='all'):
     info = {'title': title, 'artist': artist}
-    if service == 'spotify':
-        spotify = Spotify(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET)
-        return spotify.get_url(info)
-    elif service == 'tidal':
-        tidal = Tidal()
-        return tidal.get_track_url(info)
-    elif service == 'deezer':
-        deezer = Deezer()
-        return deezer.get_url(info)
-    elif service == 'apple':
-        apple = AppleMusic()
-        return apple.get_url(info)
+
+    api_map = {
+        'spotify': (Spotify, SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET),
+        'tidal': (Tidal,),
+        'deezer': (Deezer,),
+        'apple': (AppleMusic,),
+    }
+
+    if service in api_map:
+        api_args = api_map[service]
+        api_class = api_args[0]
+        api = api_class(*api_args[1:])
+        return api.get_url(info)
     else:
-        return "Something broke"
+        return "Service not supported"
+
+    # if service == 'spotify':
+    #     spotify = Spotify(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET)
+    #     return spotify.get_url(info)
+    # elif service == 'tidal':
+    #     tidal = Tidal()
+    #     return tidal.get_track_url(info)
+    # elif service == 'deezer':
+    #     deezer = Deezer()
+    #     return deezer.get_url(info)
+    # elif service == 'apple':
+    #     apple = AppleMusic()
+    #     return apple.get_url(info)
+    # else:
+    #     return "Something broke"
