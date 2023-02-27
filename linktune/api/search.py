09fd07bd24a5f59dd7e71fb224641de3372b70a4
Fiona <fiona.nugent7@gmail.com>
2023-02-27 22:01:08 +0000

    cleanup search. add youtube to search services

--- linktune/api/search.py
+++ linktune/api/search.py
@@ -4,11 +4,10 @@ from linktune.api.deezer import Deezer
 from linktune.api.applemusic import AppleMusic
 from linktune.config.config import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET
 
-# TODO: implement map of services for quicker look up; add "all" option which is the default
 def search_track(artist, title, service='all'):
     info = {'title': title, 'artist': artist}
 
-    api_map = {
+    service_map = {
         'spotify': (Spotify, SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET),
         'tidal': (Tidal,),
         'deezer': (Deezer,),
@@ -17,17 +16,15 @@ def search_track(artist, title, service='all'):
 
     if service == 'all':
         results = []
-        for service in api_map:
-            api_args = api_map[service]
-            api_class = api_args[0]
-            api = api_class(*api_args[1:])
+        for service in service_map:
+            service_class, *service_args = service_map.get(service, (None,))
+            api = service_class(*service_args)
             results.append(f"{service}: {api.get_url(info)}")
         return results
 
-    if service in api_map:
-        api_args = api_map[service]
-        api_class = api_args[0]
-        api = api_class(*api_args[1:])
+    if service in service_map:
+        service_class, *service_args = service_map.get(service, (None,))
+        api = service_class(*service_args)
         return api.get_url(info)
     else:
         return "Service not supported"
