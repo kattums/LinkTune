2e91a3af20471504ddfd4c004b99dede74cbe0c8
Fiona <fiona.nugent7@gmail.com>
2023-02-27 21:58:32 +0000

    refactor convert to use dict of tuple for services instead of long elif statements

--- linktune/api/convert.py
+++ linktune/api/convert.py
@@ -7,39 +7,31 @@ from linktune.config.config import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET
 
 class Convert:
     def __init__(self):
-        self.spotify = Spotify(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET)
-        self.tidal = Tidal()
-        self.deezer = Deezer()
-        self.apple = AppleMusic()
-        self.youtube = YouTube()
+        # Initialise a map of the service apis
+        self.service_map = {
+            'spotify': (Spotify, SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET),
+            'tidal': (Tidal,),
+            'deezer': (Deezer,),
+            'apple': (AppleMusic,),
+            'youtube': (YouTube,),
+        }
 
     def convert_link(self, link, target_service):
-        if "spotify" in link:
-            source_service = self.spotify
-        elif "tidal" in link:
-            source_service = self.tidal
-        elif "deezer" in link:
-            source_service = self.deezer
-        elif 'apple' in link:
-            source_service = self.apple
-        elif 'youtube' in link:
-            source_service = self.youtube
-        else:
-            return None
+        source_service = None
+        # check if target_service provided. use tuple unpacking to
+        # initialise class from the service map and unpack its args.
+        if target_service:
+            target_class, *target_args = self.service_map.get(target_service, (None,))
+            target_match = target_class(*target_args)
+            for service_name, service_tuple in self.service_map.items():
+                if service_name in link:
+                    source_service = service_tuple[0](*service_tuple[1:])
+                    break
 
-        track_info = source_service.get_track_info(link)
+            if source_service:
+                track_info = source_service.get_track_info(link)
+                if track_info:
+                    info = target_match.get_url(track_info)
+                    return f"{info['title']} by {info['artist']}: {info['url']}"
 
-        if not track_info:
-            return None
-
-# TODO: clean up this repetitive crap
-        if target_service == 'spotify':
-            return self.spotify.get_url(track_info)
-        elif target_service == 'tidal':
-            return self.tidal.get_url(track_info)
-        elif target_service == 'apple':
-            return self.apple.get_url(track_info)
-        elif target_service == 'youtube':
-            return self.youtube.get_url(track_info)
-        else:
-            return "Something went wrong."
+        return "Something went wrong during conversion."
