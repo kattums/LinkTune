b701f63aaa1d3025800b9ade9fa5fc364591b085
fi <fiona.nugent7@gmail.com>
2023-03-13 19:55:23 +0000

    fix(tidal): implement correct album matching in Tidal

--- linktune/api/tidal.py
+++ linktune/api/tidal.py
@@ -1,5 +1,6 @@
 from tidal_unofficial import TidalUnofficial
 import re
+import sys
 
 class Tidal:
     service_name = 'Tidal'
@@ -24,25 +25,50 @@ class Tidal:
         return track_id
 
     def get_service_url(self, info):
+        query_type = info['query_type']
+        
         title = re.sub("\(.*?\)|\[.*?\]","",info['title']).rstrip()
         if isinstance(info['artist'], list):
             artist = info['artist'][0]
         else:
             artist = info['artist']
-        query = f"{title}&artist={artist}"
+        query = f"{title}+{artist}"        
+
+        results = None
+        results = self.tidal.search(query, search_type='tracks', limit=5)
+
+        top_track = None
+        track_artist = []
+
+        # get_service_url should handle results differently depending on if its being called from a convert or search query.
+        # convert queries should attempt to match album from source url, but if they fail it should still return a track match.
+        # search queries where album is specified by the user should return track not found on {album} on {service} if album is not found.
+        
+        # check if album has been supplied either by the source service or the user
         if 'album' in info:
-            query += f"&album={info['album']}"
-
-        result = None
-        result = self.tidal.search(query, search_type='tracks', limit=1)
- # BUG: Tidal's top result when searching with album specified isnt right.
-
-        # check if we have any results, and if so take the top result returned by the API
-        if len(result['items']) > 0:
-            top_track = result['items'][0]
-            track_artist = []
-            # we loop through artists to handle the case where there are multiple artists for one song
-            for artist in top_track['artists']:
-                track_artist.append(artist['name'])
+            album = info['album'].strip()
+            for item in results['items']:
+                if artist.lower() in item['artist']['name'].lower() and title.lower() in item['title'].lower() and album.lower() in item['album']['title'].lower():
+                    top_track = item
+                    break
+            # check if top_track is empty after conclusion of the loop, then return according to query_type.
+            if top_track == None:
+                if query_type == 'search':
+                    print('could not find track on album') # TODO: handle this properly
+                else: # if query_type != search, return the top track that matches artist and title.
+                    for item in results['items']:
+                        if artist.lower() in item['artist']['name'].lower() and title.lower() in item['title'].lower():
+                            top_track = item
+                            break
+                    
+        else:
+            for item in results['items']:
+                if artist.lower() in item['artist']['name'].lower() and title.lower() in item['title'].lower():
+                    top_track = item
+                    break
+            
+        # we loop through artists to handle the case where there are multiple artists for one song
+        for artist in top_track['artists']:
+            track_artist.append(artist['name'])
             return {'service': 'Tidal', 'title': top_track['title'], 'artist': track_artist, 'url': top_track['url']}
         return f"Could not find track {title} by {artist} on Tidal."
