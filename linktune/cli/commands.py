480ac2cb4936e2067072e71e295546886f0a4130
fi <fiona.nugent7@gmail.com>
2023-03-13 21:12:53 +0000

    add(commands): error handling for invalid source url

--- linktune/cli/commands.py
+++ linktune/cli/commands.py
@@ -1,15 +1,22 @@
 from linktune.api.convert import Convert
 from linktune.api.search import search_track, pretty_print
+from linktune.utils.exceptions import *
 
 def convert(args):
     # get target service and song link from arguments
     service, link = args.target.lower(), args.link
-
+    converted_result = {}
+    
     # call convert_link function from convert api
-    converted_result = Convert().convert_link(link, service)
-    if converted_result is None:
+    try:
+        converted_result = Convert().convert_link(link, service)
+        print(Convert().pretty_print(converted_result))
+    except ServiceNotFoundException as e:
+        print(e)
+        
+    if converted_result is None: # this needs some refactoring
         print(f"Unable to find information for {link} on {service}")
-    print(Convert().pretty_print(converted_result))
+        
     return
 
 def search(args):
