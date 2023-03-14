6e6c80beab1b56e3821329b59ae6a41b092fd253
fi <fiona.nugent7@gmail.com>
2023-03-14 21:49:31 +0000

    add(exceptions): add trackidnotfound, convertlink, search, servicetimeout exceptions

--- linktune/utils/exceptions.py
+++ linktune/utils/exceptions.py
@@ -10,6 +10,9 @@ class LinkTuneException(Exception):
 class InvalidLinkException(LinkTuneException):
     """Raised when provided link is invalid."""
     
+class TrackIdNotFoundException(LinkTuneException):
+    """Raised when track ID cannot be obtained from track URL."""
+    
 class ServiceNotFoundException(LinkTuneException):
     """Raised when user specified service is not found."""
     
@@ -22,11 +25,20 @@ class TrackNotFoundException(LinkTuneException):
 class TrackNotFoundOnAlbumException(LinkTuneException):
     """Raised when track is not found on specified album."""
     
+class ConvertLinkException(LinkTuneException):
+    """Generic error raised when convert link has not returned a result object and no other exceptions were caught."""
+    
+class SearchException(LinkTuneException):
+    """Generic exception raised when search has not returned a result object and no other exceptions were caught."""
+    
 class ServiceConnectionError(LinkTuneException):
         """Raised when there is an error connecting to a service."""
 
 class ServiceRequestError(LinkTuneException):
     """Raised when there is an error with a service request."""
+
+class ServiceTimeoutError(LinkTuneException):
+    """Raised when service times out during API request."""
 
 class ServiceResponseError(LinkTuneException):
     """Raised when a service returns an unexpected response."""
