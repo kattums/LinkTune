e4d73a9f0d7614dba38803e02723022424f1523f
fi <fiona.nugent7@gmail.com>
2023-03-14 21:48:31 +0000

    add(youtubemusic): raise exceptions

--- linktune/api/youtube.py
+++ linktune/api/youtube.py
@@ -1,5 +1,7 @@
 from ytmusicapi import YTMusic
 import re
+import requests
+from linktune.utils.exceptions import *
 
 # this can convert links to both YouTube Music and regular YouTube links
 # as long as the id and externalID are the same...
@@ -10,32 +12,33 @@ class YouTube:
         self.youtube = YTMusic()
 
     def get_track_info(self, track_url):
-        print('Searching for track...')
-        track_id = self._get_track_id(track_url)
-        if not track_id:
-            return 'Could not identify YouTube Music track ID from url.' # this error doesnt work need to fix
-        track = self.youtube.search(track_id, filter='songs')[0]
-        print(track)
-        # returns dictionary with song metadata
-        artist = [artist['name'] for artist in track['artists']]
-        title = track['title']
-        album = track['album']['name']
+        try:
+            track_id = self._get_track_id(track_url)
+        except TrackIdNotFoundException as e:
+            raise e
+        
+        try:
+            track = self.youtube.search(track_id, filter='songs')[0]
+        except requests.Timeout:
+            raise ServiceTimeoutError(f"API request timed out.")
+        except IndexError:
+            raise InvalidLinkException('Could not identify track from provided link.')
 
-        # TODO: implement integration with regular YouTube
-        # external_video_id = track['microformat']
+        artist = [artist['name'] for artist in track['artists']]
+        title, album = track['title'], track['album']['name']
         return {'artist': artist, 'title': title, 'album': album}
 
-# https://music.youtube.com/watch?v=tqxRidAWER8
-    
     def _get_track_id(self, track_url):
         track_id = None
-        track_id = re.search(r"v=([\w-]+)", track_url)
-        track_id = track_id.group(1)
-
+        try:
+            track_id = re.search(r"v=([\w-]+)", track_url)
+            track_id = track_id.group(1)
+        except track_id is None:
+            raise TrackIdNotFoundException('Could not identify track ID from provided link.')
         return track_id
     
     def get_service_url(self, info):
-        title = info['title']
+        title = re.sub("\(.*?\)|\[.*?\]","",info['title']).rstrip()
         if isinstance(info['artist'], list):
             artist = info['artist'][0]
         else:
@@ -44,17 +47,15 @@ class YouTube:
         query = f"{artist} {title}"
         if 'album' in info:
             query += f" {info['album']}"
-        # store top result of search
-        top_track = self.youtube.search(f"{artist} {title}", filter = 'songs')[0]
-        track_id = top_track['videoId']
-        track_title = top_track['title']
-        track_artist = []
-        for artist in top_track['artists']:
-            track_artist.append(artist['name'])
+            
+        try:
+            top_track = self.youtube.search(f"{artist} {title}", filter = 'songs')[0]
+        except IndexError:
+            raise TrackNotFoundException(f'No results found for {title} by {artist}.')
+        except requests.Timeout:
+            raise ServiceTimeoutError(f"API request timed out.")
+        
+        track_id, track_title, track_artist = top_track['videoId'], top_track['title'], [artist['name'] for artist in top_track['artists']]
 
         return {'service': 'YouTube Music', 'title': track_title, 'artist': track_artist, 'url': f"https://music.youtube.com/watch?v={track_id}"}
-# testing using regular youtube links from unofficial accounts... actually does work but really wouldnt be that great for non-verified artists
-# I could make it take the track title from youtube and use that as the search query for other services?
-# id = 'xAok29V8Bf4'
-# yt = YouTube()
-# print(yt.get_track_info('https://music.youtube.com/watch?v=xAok29V8Bf4'))
+
