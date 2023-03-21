7495fd79118067efa6210a326e5836c04f4c0844
fi <fiona.nugent7@gmail.com>
2023-03-21 21:07:39 +0000

    add(web/js): logos for services and query on enter

--- linktune/web/static/js/index.js
+++ linktune/web/static/js/index.js
@@ -1,40 +1,86 @@
 // get elements from DOM
 const inputUrl = document.getElementById('url');
-const inputTargetService = document.getElementById('service');
 const convertButton = document.getElementById('convert');
 const info = document.getElementById('info');
 const result = document.getElementById('result');
 
-const checkboxes = document.querySelector('ul')
-let selected = [];
-// event listener for checkbox update
-checkboxes.addEventListener('change', (event) => {
-  if (event.target.type === 'checkbox') {
-    const checked = document.querySelectorAll('input[type="checkbox"]:checked')
-    selected = Array.from(checked).map(x => x.value)
-  }
-  console.log(selected);
-});
+const logos = {
+  'Spotify': '../static/img/icons/Spotify_icon.svg',
+  'Tidal': '../static/img/icons/Tidal_logo.svg',
+  'Deezer': '../static/img/icons/deezer-logo.svg',
+  'YouTube Music': '../static/img/icons/Youtube_Music_icon.svg',
+  'Apple Music': '../static/img/icons/Apple_Music_icon.svg'
+};
 
-// TODO: implement ability to search multiple services at once using checkbox selection
-// not sure if I should have it make multiple request from frontend or backend
-
-// event listener for button
+// event listeners for button click and pressing enter in input
 convertButton.addEventListener('click', (event) => {
-  // prevent default form submission behavior
-  event.preventDefault();
-
-  // get input values
-  const url = inputUrl.value;
-  const targetService = inputTargetService.value;
-
-  // send API request to backend
-  fetch(`/convert?url=${url}&target_service=${targetService}`)
-    .then(response => response.json())
-    .then(data => {
-      // update output with converted link or error message
-      info.innerText = data.info || "";
-      result.innerText = data.url || data;
-    })
-    .catch(error => console.error(error));
+  event.preventDefault(); 
+  convertUrl();
 });
+
+inputUrl.addEventListener('keydown', (event) => {
+  if (event.key === "Enter"){
+  event.preventDefault();
+  convertUrl();
+  }
+});
+
+const convertUrl = function() {
+    // clear previous search results
+    result.innerHTML = "";
+
+    // get input value
+    const source_url = inputUrl.value;
+  
+    // send API request to backend
+    fetch(`/convert?url=${source_url}`)
+      .then(response => response.json())
+      .then(data => {
+        // update output with converted link or error message
+        console.log(data);
+        console.log(data.service_url);
+        const infoText = `${data.title} by ${data.artist}`
+        info.innerText = infoText || "";
+        const service_urls = data.service_url;
+  
+  
+        // Map results data to result elements
+        service_urls.map(service_url => {
+          let result_section = document.createElement('section');
+          result_section.classList.add('h-10', 'bg-blue-300', 'my-2', 'rounded-lg', 'text-slate-700', 'flex', 'flex-row');
+  
+          // Get the service name and result from the service_url object
+          const serviceName = Object.keys(service_url)[0];
+          const resultUrl = Object.values(service_url)[0];
+  
+          // Create a new element to contain logo and service name
+          // and elements for logo and service name
+          let logoWrapper = document.createElement('div');
+          logoWrapper.classList.add('h-10', 'bg-gray-300', 'rounded', 'p-2', 'flex', 'items-center');
+          
+          let logoImg = document.createElement('img');
+          logoImg.src = logos[serviceName];
+          logoImg.alt = serviceName;
+          logoImg.classList.add('py-0.5', 'h-10');
+  
+          let serviceNameElem = document.createElement('span');
+          serviceNameElem.innerText = serviceName;
+          serviceNameElem.classList.add('pl-px', 'text-gray-700');
+  
+          logoWrapper.appendChild(logoImg);
+          logoWrapper.appendChild(serviceNameElem);
+  
+          // Create a new element for the track result
+          let resultElem = document.createElement('span');
+          resultElem.classList.add('pl-2')
+          resultElem.innerText = resultUrl;
+  
+          // Append logo, service name, track result elements to the result_section
+          result_section.appendChild(logoWrapper);
+          result_section.appendChild(resultElem);
+  
+          result.appendChild(result_section);
+        });
+      })
+      .catch(error => console.error(error));
+};
